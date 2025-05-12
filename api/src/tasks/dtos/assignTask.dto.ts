2e8d0b24aee437cbbb4a4bd1f15c1f1107253b35
j3drzejo <jirla30@gmail.com>
2025-05-12 19:10:43 +0200

    unused dtos

--- api/src/tasks/dtos/assignTask.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
-
-export class AssignTaskDto {
-  @IsNotEmpty()
-  @IsNumber()
-  userId: number;
-}
--- api/src/tasks/dtos/index.ts
+++ api/src/tasks/dtos/index.ts
@@ -1,12 +1,5 @@
 import { CreateTaskDto } from './createTask.dto';
 import { UpdateTaskDto } from './updateTask.dto';
 import { MoveTaskDto } from './moveTask.dto';
-import { AssignTaskDto } from './assignTask.dto';
 import { AssignUserDto } from './assignUser.dto';
-export {
-  CreateTaskDto,
-  UpdateTaskDto,
-  MoveTaskDto,
-  AssignTaskDto,
-  AssignUserDto,
-};
+export { CreateTaskDto, UpdateTaskDto, MoveTaskDto, AssignUserDto };
